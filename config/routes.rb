41cdbaf2b4b82d339ca88a1f8021dab83c2f48a6
Nina Witek <nina.witek@gmail.com>
2017-03-28 19:16:19 +0200

    Fix crumbs and path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :user
-  
+
   root to: 'students#index'
 
   resources :teachers
-  resources :report_subjects
+  resources :reports do
+    get :subjects, on: :collection
+  end
   resources :students do
     get :subjects
   end
