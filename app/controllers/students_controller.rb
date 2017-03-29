3c152934abbdd0f986b852113028d283d17c4a03
Nina Witek <nina.witek@gmail.com>
2017-03-29 14:26:26 +0200

    Display avg_notes and pemit to persist subject_item_ids for student model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,6 @@ class StudentsController < ApplicationController
   expose(:students)
   expose(:student, attributes: :student_params)
   expose(:student_subject_items) { student.subject_items }
-
   def create
     if student.save
       redirect_to student_path(student), notice: I18n.t('shared.created', resource: 'Student')
@@ -27,6 +26,6 @@ class StudentsController < ApplicationController
   private
 
   def student_params
-    params.require(:student).permit(:first_name, :last_name)
+    params.require(:student).permit(:first_name, :last_name, subject_item_ids: [])
   end
 end
