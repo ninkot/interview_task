5d1ba272cd4d1cb9b17f0a9db2ecf8eb529f893c
Nina Witek <nina.witek@gmail.com>
2017-03-30 22:39:22 +0200

    Add some test for payments resources

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'database columns' do
+    it { should have_db_column :student_id }
+    it { should have_db_column :deadline }
+    it { should have_db_column :paid_at }
+    it { should have_db_column :price }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :student }
+  end
 end
